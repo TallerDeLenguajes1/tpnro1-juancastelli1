69ac6f80d73d0999b8f854dc1c8a5e334a219e34
juancastelli1 <juancastelli12345@gmail.com>
2020-09-09 14:47:11 -0300

    Error FO

---
+++ TP01/TP01/Helper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP01
+{
+    static class Helper
+    {
+        public static Tareas BusquedaPorId(int idTarea, List<Tareas> listatarea)
+        {
+            foreach (Tareas tarea in listatarea) 
+            {
+                if (tarea.ID1 == idTarea) 
+                {
+                    return tarea;
+                }
+            }
+            return null;
+        }
+        public static int ContarPosicion(int idTarea, List<Tareas> listatarea)
+        {
+            int t = 0;
+            foreach (Tareas tarea in listatarea)
+            {
+                if (tarea.ID1 == idTarea)
+                {
+                    return t;
+                }
+                t++;
+            }
+            return -1;
+        }
+        public static Tareas BusquedaPorPalabra(string palabra, List<Tareas> listatarea)
+        {
+            foreach (Tareas tarea in listatarea)
+            {
+                if (tarea.Descripcion1.Contains(palabra))
+                {
+                    return tarea;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ TP01/TP01/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string nombreA;
+            string apellidoA;
+            int DNIA;
+            List<Tareas> tareasA = new List<Tareas>();
+            List<Tareas> tareasReal = new List<Tareas>();
+            Console.WriteLine("Ingrese nombre Nuevo Empleado");
+            nombreA = Console.ReadLine();
+            Console.WriteLine("Ingrese apellido Nuevo Empleado");
+            apellidoA = Console.ReadLine();
+            Console.WriteLine("Ingrese DNI Nuevo Empleado");
+            DNIA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de Tareas a Cargar");
+            n = int.Parse(Console.ReadLine());
+            int IDT;
+            string DESCT;
+            int DURT;
+            for (int i = 0; i < n; i++) 
+            {
+                IDT = i;
+                Console.WriteLine("Ingrese Descripcion Tarea " + i + ":");
+                DESCT = Console.ReadLine();
+                Console.WriteLine("Ingrese Duracion Tarea " + i + ":");
+                DURT = int.Parse(Console.ReadLine());
+                while (DURT < 9 | DURT > 100) {
+                    Console.WriteLine("No es una duracion valida. Ingrese Otra");
+                    DURT = int.Parse(Console.ReadLine());
+                };
+                tareasA.Add(new Tareas(IDT, DESCT, DURT));
+                
+            }
+            Empleado EmpleadoE = new Empleado(DNIA, nombreA, apellidoA, tareasA, tareasReal);
+            Console.WriteLine("Tareas Pendientes:");
+            foreach (Tareas T in EmpleadoE.TareasPendientes1)
+            {
+                T.MostrarTarea();
+            }
+            for (int h = 0; h < n; h++)
+            {
+                Console.Write("Tarea "+ h +":" + "\n");
+                EmpleadoE.TareasPendientes1[h].MostrarTarea();
+                Console.Write("Esta tarea fue realizada?(S/N):");
+                if (Console.ReadLine() == "S")
+                {
+                    EmpleadoE.PasarTarea(EmpleadoE.TareasPendientes1[h].ID1);
+                }
+            }
+            Console.WriteLine("Tareas Realizadas:");
+            foreach (Tareas T in EmpleadoE.TareasRealizadas1)
+            {
+                T.MostrarTarea();
+            }
+            Console.WriteLine("Tareas Pendientes:");
+            foreach (Tareas T in EmpleadoE.TareasPendientes1)
+            {
+                T.MostrarTarea();
+            }
+        }
+    }
+}
